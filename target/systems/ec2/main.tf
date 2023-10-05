0aed3bda48a43abbad25227be979335388d1018e
etechstaff <136949110+etechstaff@users.noreply.github.com>
2023-10-05 22:05:25 +0200

    Update main.tf

--- target/systems/ec2/main.tf
+++ target/systems/ec2/main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "server" {
+  source = "https://github.com/etechstaff/etech-team6-base-iac/tree/main//modules/ec2"
+  num    = 5
+}
